2f6a7688134ac200083aad30e133f2ee3870f94e
Gabrielly <gabriellymarques624@gmail.com>
2023-01-31 02:31:15 -0300

    desenvolvendo o desafio abstraindo um bootcamp

---
+++ Java/Orange-Tech-BackEnd-Bootcamp/DesafioPOO/AbstraindoUmBootcamp/src/Inicio.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+import dominio.*;
+
+public class Inicio{
+    public static void main(String [] args){
+        Curso curso1 = new Curso(); //instanciando a classe Curso
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("aprendendo java");
+        curso1.setcargaHoraria(8);
+
+        Curso curso2 = new Curso(); //instanciando a classe Curso
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("aprendendo js");
+        curso2.setcargaHoraria(4);
+
+        
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Tirando dúvidas em Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+    
+}
